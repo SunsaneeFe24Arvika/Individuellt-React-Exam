5682bc105833de51c15bbb312a6cc2a3dd5185bb
Sunsanee Persson <3kdyhfe24.perssu@folkuniversitetet.nu>
2025-05-02 23:32:27 +0200

    lägga till knappanar

--- EVENTS-APP/src/stores/counter.js
+++ EVENTS-APP/src/stores/counter.js
@@ -1,13 +1,29 @@
 import { create } from "zustand";
 
-const useTicketStore = create ((set) => ({
+const useTicketStore = create((set) => ({
     ticket : 0,
+    price : 0,
+    totalPrice : 0,
+    setPrice : (newPrice) => {
+        set((state) => ({
+            price : newPrice,
+            totalPrice : state.ticket * newPrice, //Återställ totalpris när priset uppdateras
+        }));
+    },
     increment : () => {
-        set((state) => ({ticket : state.ticket + 1}));
+        set((state) => ({
+            ticket : state.ticket + 1,
+            totalPrice : (state.ticket + 1) * state.price,
+        }));
+        
     },
     decrement : () => {
-        set((state) => (state.ticket > 0 ? {ticket: state.ticket - 1} : {ticket: 0}));
-    }
+        set((state) => ({
+            ticket : state.ticket > 0 ? state.ticket - 1 : 0,
+            totalPrice : state.ticket > 0 ? (state.ticket -1) * state.price : 0,
+         }));
+       
+    },
 }));
 
 export default useTicketStore;
